f3c1f106ad52ad92a3e005229952f4ffd57cdd50
Camilo Guerrero <stratocagm@gmail.com>
2020-12-30 14:17:43 -0500

    https://github.com/kmilog/VieCli.git

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Mi contador: {{contador}}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mapState} from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  computed:{
+    ...mapState(['contador'])
   }
 }
 </script>
