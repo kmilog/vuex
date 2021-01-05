fb360274ca933bfbb977560ac3501391a13e9e9e
stratocagm <stratocagm@gmail.com>
2021-01-05 12:28:32 -0500

    finalizacion

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,27 @@ export default createStore({
     contador: 100
   },
   mutations: {
+    incrementar(state, payLoad){
+      state.contador = state.contador + payLoad
+    },
+    disminuir(state, payLoad){
+      state.contador = state.contador - payLoad
+    }
   },
   actions: {
+    accionIncrementar({commit}){
+      commit('incrementar',10)
+    },
+    accionDisminuir({commit}, numero){
+      commit('disminuir', numero)
+    },
+    accionBoton({commit}, objeto){
+      if(objeto.estado){
+        commit('incrementar', objeto.numero)
+      }else{
+        commit('disminuir', objeto.numero)
+      }
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Mi contador: {{contador}}</h1>
+    <h1
+      :style="colorContador">Mi contador: {{contador}}</h1>
+    <button @click="accionIncrementar">Aumentar</button>
+    <BtnDisminur ></BtnDisminur>
+    <hr>
+    <BtnAccion :estado="true"></BtnAccion>
+    <BtnAccion :estado="false"></BtnAccion>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import {mapState} from 'vuex'
+import {mapState, mapMutations, mapActions} from 'vuex'
+import BtnDisminur from '../components/BtnDisminur'
+import BtnAccion from '../components/BtnAccion'
 
 export default {
   name: 'Home',
   components: {
+    BtnDisminur,
+    BtnAccion
   },
   computed:{
-    ...mapState(['contador'])
+    ...mapState(['contador']),
+    colorContador(){
+       return [ this.contador > 100 ? {'color': 'green'} : {'color' : 'red'}]
+    }
+  },
+  methods: {
+    //No se recomienda mapear las mutaciones
+    ...mapMutations(['incrementar']),
+    ...mapActions(['accionIncrementar'])
   }
 }
 </script>
